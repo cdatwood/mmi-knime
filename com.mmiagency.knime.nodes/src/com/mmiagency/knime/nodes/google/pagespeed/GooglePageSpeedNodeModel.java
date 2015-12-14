c36d1774e0946735eb8d1e11d9017fe8098e6a6a
Ed Ng <ed@wellframe.com>
2015-12-14 15:39:30 -0500

    fixed google pagespeed in KNIME 3.1 due to a change in jackson library

--- com.mmiagency.knime.nodes/src/com/mmiagency/knime/nodes/google/pagespeed/GooglePageSpeedNodeModel.java
+++ com.mmiagency.knime.nodes/src/com/mmiagency/knime/nodes/google/pagespeed/GooglePageSpeedNodeModel.java
@@ -58,7 +58,7 @@ import com.google.api.client.http.HttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.JsonObjectParser;
-import com.google.api.client.json.jackson.JacksonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.util.Key;
 
 
