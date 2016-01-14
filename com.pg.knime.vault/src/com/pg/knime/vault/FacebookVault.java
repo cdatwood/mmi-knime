41bb081efec450bf9d4400282bec023d725270b1
Ed Ng <ed@wellframe.com>
2016-01-14 10:44:00 -0500

    switched to Ed's app id

--- com.pg.knime.vault/src/com/pg/knime/vault/FacebookVault.java
+++ com.pg.knime.vault/src/com/pg/knime/vault/FacebookVault.java
@@ -4,10 +4,10 @@ import com.pg.knime.secure.DefaultVault;
 
 public class FacebookVault extends DefaultVault {
 
-    private static String APP_ID = "959814390733501";
-    private static String APP_SECRET = "46ace08cce9c040eca0dbd080d83c252";
-    //private static String APP_ID = "128490190584651";
-    //private static String APP_SECRET = "ebcf28fe6505902c967f60ba40fbd7d0";
+    //private static String APP_ID = "959814390733501";
+    //private static String APP_SECRET = "46ace08cce9c040eca0dbd080d83c252";
+    private static String APP_ID = "128490190584651";
+    private static String APP_SECRET = "ebcf28fe6505902c967f60ba40fbd7d0";
 
     public FacebookVault() {
             this.addKey("DEFAULT_APP_ID", APP_ID);
